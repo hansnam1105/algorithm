ae9a9e7bb2a978741f94c2352a7f690100c878c0
HanSolo.CAU <34000255+hansnam1105@users.noreply.github.com>
2024-08-14 15:28:38 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 103 ms, Memory: 18,668 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -1,23 +1,32 @@
+/**
+  * 1218. [S/W 문제해결 기본] 4일차 - 괄호 짝짓기 D4
+  * 메모리 : 
+  * 실행시간 : 
+  * 
+  * 스택을 활용한 괄호 짝짓기
+  */
+
 import java.util.*;
 import java.io.*;
  
 public class Solution {
 	 
 	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
+		// 괄호 확인 용 배열
 		char[] parent= {'(', ')', '[', ']', '{', '}', '<', '>'};
 
 
 		for(int T=1; T<=10; T++) {
 			int tc = Integer.parseInt(br.readLine());
 			String parentInput = br.readLine();
-			char[] arr = new char[tc];
 			Stack<Character> stk = new Stack<>(); 
+			
 			for(int i=0; i<tc; i++) {
+				// 현재 입력받은 값
 				char cur = parentInput.charAt(i);
+				//괄호 존재시 pop
 				if(cur == parent[1] && stk.peek().equals(parent[0])) {
 					stk.pop();
 				}
@@ -41,7 +50,8 @@ public class Solution {
 			}
 			
 		}
-        System.out.println(sb);
+		
+		System.out.println(sb);
 	}
 
 }
