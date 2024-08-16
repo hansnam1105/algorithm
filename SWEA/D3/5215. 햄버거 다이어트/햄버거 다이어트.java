667b1a96824a64fc1a33b3506418b895e8f6c6f0
HanSolo.CAU <34000255+hansnam1105@users.noreply.github.com>
2024-08-16 11:30:27 +0900

    [D3] Title: 햄버거 다이어트, Time: 204 ms, Memory: 20,348 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,5 +1,5 @@
 /**
- * 햄버거 다이어트 [조합]
+ * 햄버거 다이어트 [부분집합]
  */
 
 import java.io.*;
@@ -11,14 +11,11 @@ public class Solution {
 	static int N, L;
 	static int maxTaste = 0;
 	static int[][] material;
-	static int[][] isSelected;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
-		
 		
 		int T = parseInt(br.readLine());
 		for(int tc=1; tc<=T; tc++) {
@@ -27,24 +24,23 @@ public class Solution {
 			L = parseInt(st.nextToken());
 			
 			material = new int[N][2];
-			isSelected = new int[N][2];
 			
 			for(int i=0; i<N; i++) {
 				st = new StringTokenizer(br.readLine());
 				material[i][0] = parseInt(st.nextToken());
 				material[i][1] = parseInt(st.nextToken());
 			}
-			maxTaste = 0;
-			comb(0, 0, 0);
 			
+			maxTaste = 0;
+			boolean[] isSelected = new boolean[N];
+			powerSet(0, 0, 0, isSelected);	
 			sb.append("#").append(tc).append(" ").append(maxTaste).append("\n");
 			
 		}
-		
 		System.out.println(sb);
 	}
 	
-	public static void comb(int count, int taste, int calorie) {
+	public static void powerSet(int count, int taste, int calorie, boolean[] isSelected) {
 
 		if(calorie>L) {
 			return;
@@ -56,8 +52,10 @@ public class Solution {
 			return;
 		}
 		
-		comb(count+1, taste+material[count][0], calorie+material[count][1]);
-		comb(count+1, taste, calorie);
+		isSelected[count] = true;
+		powerSet(count+1, taste+material[count][0], calorie+material[count][1], isSelected);
+		isSelected[count] = false;
+		powerSet(count+1, taste, calorie, isSelected);
 		
 		
 	}
