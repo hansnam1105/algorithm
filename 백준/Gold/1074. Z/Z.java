1b412b84d43188601ed1e50e68e14d90ac95e97d
HanSolo.CAU <34000255+hansnam1105@users.noreply.github.com>
2024-08-21 14:42:24 +0900

    [Gold V] Title: Z, Time: 68 ms, Memory: 11620 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+import static java.lang.Integer.parseInt;
+
+public class Main {
+	
+	static int ans;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = parseInt(st.nextToken());
+		int r = parseInt(st.nextToken());
+		int c = parseInt(st.nextToken());
+		
+		int mapLength = (int) Math.pow(2, N);
+				
+		ans = 0;
+		searchSpace(mapLength, r, c);
+		
+		bw.write(String.valueOf(ans));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	
+	static void searchSpace(int n, int r, int c) {
+		int size = n/2;
+		// 2 3 1
+		
+		if(n == 1) {
+			return;
+		} else {
+		    
+			
+			// 사분면 위치
+			// 2 3 1 일시 3>1 1
+			// size = 1
+			// searchSpace(1, 1, 1)
+			// ans += 8
+			// ans += 3
+			if(r<size && c<size) {
+				searchSpace(size, r, c);
+			}
+			else if(r<size && c>=size) {
+				ans += n*n/4;
+				searchSpace(size, r, c-size);
+			}
+			else if(r>=size && c<size) {
+				ans += 2*(n*n/4);
+				searchSpace(size, r-size, c);
+			}
+			else if(r>=size && c>=size) {
+				ans += 3*(n*n/4);
+				searchSpace(size, r-size, c-size);
+			}
+			
+			
+			
+		}
+	}
+	
+	
+	
+	
+
+}
