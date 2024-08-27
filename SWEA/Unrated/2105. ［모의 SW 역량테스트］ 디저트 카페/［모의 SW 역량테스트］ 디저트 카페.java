2a176caccd55bfc4e1df5b43ab088356ac2b7389
HanSolo.CAU <34000255+hansnam1105@users.noreply.github.com>
2024-08-27 15:38:19 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 422 ms, Memory: 93,500 KB -BaekjoonHub

--- SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
@@ -8,71 +8,82 @@ import java.util.*;
 public class Solution {
 
 	static int N, maxSize, startR, startC;
+
 	// 탐색 순서
-	static int[] dr = {1, 1, -1, -1};
-	static int[] dc = {1, -1, -1, 1};
-	
+	static int[] dr = { 1, 1, -1, -1 };
+	static int[] dc = { 1, -1, -1, 1 };
+
 	static int[][] map;
 	static boolean[][] visited;
 	static HashSet<Integer> dessert;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine());
-		for(int tc=1; tc<=T; tc++) {
+		for (int tc = 1; tc <= T; tc++) {
 			N = Integer.parseInt(br.readLine());
 			map = new int[N][N];
-			
-			
-			for(int i=0; i<N; i++) {
+
+			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
-				for(int j=0; j<N; j++) {
+				for (int j = 0; j < N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 			maxSize = -1;
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<N; j++) {
-					visited = new boolean[N][N];
-					dessert = new HashSet<>();		
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					visited = new boolean[N][N]; // 방문 여부 확인
+					dessert = new HashSet<>(); // 방문 카페 디저트 수 등록
+					// 시작 좌표
 					startR = i;
 					startC = j;
 					dfs(0, i, j, 0, 0);
 				}
 			}
-			
+
 			sb.append("#").append(tc).append(" ").append(maxSize).append("\n");
 		}
 		System.out.println(sb);
 
 	}
-	
+
 	public static void dfs(int depth, int r, int c, int dir, int turnCount) {
-		if(depth>0 && r == startR && c == startC && turnCount >= 3) {
+		// depth - 카페 방문 횟수 (return 값)
+		// turnCount - 사각형 -> 3번 방향을 꺾어서 도착
+		if (depth > 0 && r == startR && c == startC && turnCount == 3) {
 			maxSize = Math.max(maxSize, depth);
 			return;
 		}
 		
-		for(int i=dir; i<4; i++) {
+
+		// 한번 꺽은 방향은 다시 못 가도록 dir 활용
+		for (int i = dir; i < 4; i++) {
 			int nr = r + dr[i];
 			int nc = c + dc[i];
-			if(check(nr, nc) && !dessert.contains(map[nr][nc])) {
+			// 카페 투어 중에 같은 숫자의 디저트를 팔고 있는 카페가 있으면 안 된다
+			if (check(nr, nc) && !dessert.contains(map[nr][nc])) {
 				visited[nr][nc] = true;
 				dessert.add(map[nr][nc]);
-				dfs(depth+1, nr, nc, i, turnCount + (dir!=i ? 1 : 0));
+                
+				dfs(depth + 1, nr, nc, i, turnCount + (dir != i ? 1 : 0));
+				// 백트레킹 다시 다른 경로를 탐색하기 위해
 				dessert.remove(map[nr][nc]);
-				visited[nr][nc] = false;
+				visited[nr][nc] = false; // 이전 경로에서 방문했던 좌표는 다시 탐색할 수 있어야 한다
 			}
-			
+            
+
 		}
 	}
-	
+
+	// map 안을 탐색하는지 & 새로운 곳을 탐색하는지
 	public static boolean check(int r, int c) {
-        if (r >= 0 && c >= 0 && r < N && c < N && !visited[r][c]) {
-            return true;
-        }
-        return false;
-    }
+		if (r >= 0 && c >= 0 && r < N && c < N && !visited[r][c]) {
+			return true;
+		}
+		return false;
+	}
 
 }
