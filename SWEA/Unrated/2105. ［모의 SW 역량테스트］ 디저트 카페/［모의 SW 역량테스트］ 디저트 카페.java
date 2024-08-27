bb97eee0309163da420c1d108b5ae053161d4e56
HanSolo.CAU <34000255+hansnam1105@users.noreply.github.com>
2024-08-27 17:08:13 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 190 ms, Memory: 29,484 KB -BaekjoonHub

--- SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
@@ -9,13 +9,13 @@ public class Solution {
 
 	static int N, maxSize, startR, startC;
 
-	// 탐색 순서
+	// 탐색 순서: 우하, 좌하, 좌상, 우상
 	static int[] dr = { 1, 1, -1, -1 };
 	static int[] dc = { 1, -1, -1, 1 };
 
 	static int[][] map;
 	static boolean[][] visited;
-	static HashSet<Integer> dessert;
+	static boolean[] dessert;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -36,7 +36,7 @@ public class Solution {
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
 					visited = new boolean[N][N]; // 방문 여부 확인
-					dessert = new HashSet<>(); // 방문 카페 디저트 수 등록
+					dessert = new boolean[101]; // 방문 카페 디저트 수 등록
 					// 시작 좌표
 					startR = i;
 					startC = j;
@@ -57,27 +57,27 @@ public class Solution {
 			maxSize = Math.max(maxSize, depth);
 			return;
 		}
-     
+		
+
 		// 한번 꺽은 방향은 다시 못 가도록 dir 활용
 		for (int i = dir; i < 4; i++) {
 			int nr = r + dr[i];
 			int nc = c + dc[i];
 			// 카페 투어 중에 같은 숫자의 디저트를 팔고 있는 카페가 있으면 안 된다
-			if (check(nr, nc) && !dessert.contains(map[nr][nc])) {
+			if (check(nr, nc) && !dessert[map[nr][nc]]) {
+				//
 				visited[nr][nc] = true;
-				dessert.add(map[nr][nc]);
-                
+				dessert[map[nr][nc]] = true;
 				dfs(depth + 1, nr, nc, i, turnCount + (dir != i ? 1 : 0));
 				// 백트레킹 다시 다른 경로를 탐색하기 위해
-				dessert.remove(map[nr][nc]);
+				dessert[map[nr][nc]] = false;
 				visited[nr][nc] = false; // 이전 경로에서 방문했던 좌표는 다시 탐색할 수 있어야 한다
 			}
-            
-            // 사각형을 만들기 위해 두 방향까지만 시작하도록 제한
+			
+			// 사각형을 만들기 위해 두 방향까지만 시작하도록 제한
             if (i == 1 && dir == 0) {
                 break;
             }
-            
 
 		}
 	}
