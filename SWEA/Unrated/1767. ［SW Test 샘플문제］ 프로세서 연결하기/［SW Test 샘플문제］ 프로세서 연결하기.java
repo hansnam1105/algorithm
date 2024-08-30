f0485e2dfbcc497ea878389d2bbb2b6af82bf215
HanSolo.CAU <34000255+hansnam1105@users.noreply.github.com>
2024-08-30 15:45:48 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 209 ms, Memory: 24,788 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -1,129 +1,120 @@
-import java.util.Scanner;
-import java.io.FileInputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.awt.Point;
+import java.io.*;
+import java.util.*;
 
-class Solution
-{
+public class Solution {
+
     static class Core {
-		
-		int y, x;
-		
-		
-		public Core(int y, int x) {
-			this.y = y;
-			this.x = x;
-		}
-	}
-    
-    static StringBuilder sb = new StringBuilder();
-	static int[][] core;
-	static int N, minWireLength, maxCore;
-	static List<Core> coreList;
-	static int[] dy = {1, -1, 0, 0};
-	static int[] dx = {0, 0, 1, -1};
+        Point p;
 
-    
-	public static void main(String args[]) throws Exception
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int tc = Integer.parseInt(br.readLine());
-		for(int t=1; t<=tc; t++) {
-			// 7<= N <= 12
-			N  = Integer.parseInt(br.readLine());
-			core = new int[N][N];
-			coreList =  new ArrayList<>();
-			// 0은 빈 cell, 1은 core의미
-			// core은 1<= core <= 12
-			for(int i=0; i<N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for(int j=0; j<N; j++) {
-					int c = Integer.parseInt(st.nextToken());
-					if(c == 1) {
-						core[i][j] = 1;
-						
-						if(i==0 || j==0 || i==N-1 || j==N-1) {
-							continue;							
-						}
-						//coreList 에 core 값 추가
-						coreList.add(new Core(i,j));
-					} //if end
-				}
-			} //for end
-			
-			minWireLength = Integer.MAX_VALUE;
-			maxCore = Integer.MIN_VALUE;
-			
-			search(0, 0, 0);
-			
-			sb.append("#"+t+" "+minWireLength+"\n");
-			
-		}	
-		
-		System.out.println(sb);
-	}
-    
-    public static void search(int idx, int coreCnt, int wireCnt) {
-		if(idx == coreList.size()) {
-			if(maxCore<coreCnt) {
-				maxCore = coreCnt;
-				minWireLength = wireCnt;
-			} else if(maxCore == coreCnt) {
-				minWireLength = Math.min(wireCnt, minWireLength);
-			}
-			return;
-		}
-		
-		int x = coreList.get(idx).x;
-		int y = coreList.get(idx).y;
-		
-		for(int i=0; i<4; i++) {
-			int count = 0, nx = x, ny = y;
-			
-			while(true) {
-				nx+=dx[i];
-				ny+=dy[i];
-				
-				if(nx<0 || nx>=N || ny<0 || ny>=N) {
-					break;
-				}
-				if(core[ny][nx] == 1) {
-					count = 0;
-					break;
-				}
-				count++;
-			}
-			
-			int final_x = x;
-			int final_y = y;
-			
-			for(int j=0; j<count; j++) {
-				final_x += dx[i];
-				final_y += dy[i];
-				
-				core[final_y][final_x] = 1;
-			}
-			
-			if(count==0) {
-				search(idx+1, coreCnt, wireCnt);
-			}
-			else {
-				search(idx+1, coreCnt+1, wireCnt+count);
-			}
-			
-			final_x = x;
-			final_y = y;
-			
-			for(int j=0; j<count; j++) {
-				final_x += dx[i];
-				final_y += dy[i];
-				
-				core[final_y][final_x] = 0;
-			}
-		}
-	}
+        public Core(Point p) {
+            this.p = p;
+        }
+    }
+
+    static int N, maxCore, minWire;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static int[][] map;
+    static ArrayList<Core> coreList;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            N = Integer.parseInt(br.readLine());
+            map = new int[N][N];
+            coreList = new ArrayList<>();
+            int temp = 0;
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    temp = Integer.parseInt(st.nextToken());
+                    if (temp == 1) {
+                        map[i][j] = 1;
+                        if (i == 0 || j == 0 || i == N - 1 || j == N - 1) {
+                            continue;
+                        }
+                        coreList.add(new Core(new Point(i, j)));
+                    }
+                }
+            }
+
+            maxCore = Integer.MIN_VALUE;
+            minWire = Integer.MAX_VALUE;
+
+            dfs(0, 0, 0);
+
+            sb.append("#").append(tc).append(" ").append(minWire).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int idx, int coreCnt, int wireCnt) {
+        if (idx == coreList.size()) {
+            if (coreCnt > maxCore) {
+                maxCore = coreCnt;
+                minWire = wireCnt;
+            } else if (coreCnt == maxCore) {
+                minWire = Math.min(minWire, wireCnt);
+            }
+            return;
+        }
+
+        Core cur = coreList.get(idx);
+        int cr = cur.p.x;
+        int cc = cur.p.y;
+
+        boolean isConnected = true;
+        for (int i = 0; i < 4; i++) {
+            int count = 0, nr = cr, nc = cc;
+            isConnected = false;
+            while (true) {
+                nr += dr[i];
+                nc += dc[i];
+
+                if (nr < 0 || nr >= N || nc < 0 || nc >= N) {
+                    break;
+                }
+                if (map[nr][nc] == 1) {
+                    count = 0;
+                    break;
+                }
+
+                count++;
+            }
+
+            if (count > 0) {
+                isConnected = true;
+
+                int installWireRow = cr;
+                int installWireCol = cc;
+
+                for (int j = 0; j < count; j++) {
+                    installWireRow += dr[i];
+                    installWireCol += dc[i];
+                    map[installWireRow][installWireCol] = 1;
+                }
+
+                dfs(idx + 1, coreCnt + 1, wireCnt + count);
+
+                installWireRow = cr;
+                installWireCol = cc;
+
+                for (int j = 0; j < count; j++) {
+                    installWireRow += dr[i];
+                    installWireCol += dc[i];
+                    map[installWireRow][installWireCol] = 0;
+                }
+            }
+        }
+
+        dfs(idx + 1, coreCnt, wireCnt);
+        
+    }
 }
