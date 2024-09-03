286c35f5a75e8d44ba13dfd9f1c2d4ce6a42d285
HanSolo.CAU <34000255+hansnam1105@users.noreply.github.com>
2024-09-03 16:24:28 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 265 ms, Memory: 86,888 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -1,42 +1,54 @@
+
+/**
+ * 배열을 이용한 단순 스왑핑
+ * DFS 활용해서 모든 경우의 수 확인
+ * 최대 교환했을시 최댓값 구한다
+ */
+import java.io.*;
 import java.util.*;
 
 public class Solution {
 
-	static String[] arr;
+	static char[] arr;
     static int max, chance;
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int T=Integer.parseInt(br.readLine());
 		
-		for(int test_case = 1; test_case <= T; test_case++)
+		for(int tc = 1; tc <= T; tc++)
 		{
-			arr= sc.next().split("");
-            chance = sc.nextInt();
+			st = new StringTokenizer(br.readLine());
+			arr= st.nextToken().toCharArray();
+            chance = Integer.parseInt(st.nextToken());
             
             max = 0;
             if(arr.length < chance) {
                 chance = arr.length;
             }
             dfs(0, 0);
-            System.out.println("#" + test_case + " " + max);
+            bw.write("#" + tc + " " + max + "\n");
             
 		}
+		bw.flush();
+		bw.close();
+		br.close();
 
 	}
 	
 	static void dfs(int start, int cnt) {
         if(cnt == chance) {
             String result = "";
-            for(String s : arr)
+            for(char s : arr)
                 result += s;
             max = Math.max(max, Integer.parseInt(result));
             return;
         }
         for(int i = start; i<arr.length; i++) {
             for(int j = i+1; j<arr.length; j++){
-                String temp = arr[i];
+                char temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
                 
