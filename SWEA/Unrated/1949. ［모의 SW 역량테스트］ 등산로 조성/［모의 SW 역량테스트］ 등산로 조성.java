9627c4123cd76b79d2b03c98e95270887fe78639
HanSolo.CAU <34000255+hansnam1105@users.noreply.github.com>
2024-09-04 15:50:16 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 136 ms, Memory: 18,660 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -1,88 +1,93 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Solution {
-	
-	static int N, K, maxHeight, maxRoute;
-	static int[] dy = {1, -1, 0, 0};
-	static int[] dx = {0, 0, 1, -1};
-	static StringBuilder sb = new StringBuilder();
-	static int[][] mountain;
+
+	static int N, K, maxRoute;
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	static int[][] map;
 	static boolean[][] visited;
-	
-	class Top{
-		int x, y;
-		
-		public Top(int y, int x) {
-			this.y = y;
-			this.x = x;
-		}
-	}
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		
 		int T = Integer.parseInt(br.readLine());
-		int tempK = K;
-
 		
 		for(int tc=1; tc<=T; tc++) {
 			st = new StringTokenizer(br.readLine());
+			
 			N = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
-			maxHeight = 1;
-			maxRoute = 0;
-			mountain = new int[N][N];
-			visited = new boolean[N][N];
+			
+			map = new int[N][N];
+			
+			int maxHeight = 1;
 			for(int i=0; i<N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for(int j=0; j<N; j++) {
-					mountain[i][j] = Integer.parseInt(st.nextToken());
-					maxHeight = Math.max(maxHeight, mountain[i][j]);
+					map[i][j] = Integer.parseInt(st.nextToken());
+					maxHeight = Math.max(maxHeight, map[i][j]);
 				}
 			}
+			
+			maxRoute = Integer.MIN_VALUE;
+			visited = new boolean[N][N];
 			for(int i=0; i<N; i++) {
 				for(int j=0; j<N; j++) {
-					if(mountain[i][j] == maxHeight) {
+					if(map[i][j] == maxHeight) {
 						visited[i][j] = true;
-						dfs(i, j, 1, 1);
+						dfs(1, i, j, 1);
 						visited[i][j] = false;
 					}
 				}
 			}
-			sb.append("#" + tc + " " + maxRoute + "\n");
+			
+			bw.write("#"+tc+" "+maxRoute);
+			bw.newLine();
+			
+			
 		}
-		System.out.println(sb);
+		bw.flush();
+		bw.close();
+		br.close();
+
+	}
+	
+	public static void dfs(int depth, int r, int c, int kUsed) {
+		int curLoc = map[r][c];
+		for(int d=0; d<4; d++) {
+			int nr = r + dr[d];
+			int nc = c + dc[d];
+			if(check(nr, nc)) {
+				if(map[nr][nc] < curLoc) {
+					visited[nr][nc] = true;
+					dfs(depth+1, nr, nc, kUsed);
+					visited[nr][nc] = false;
+				} 
+				else if(kUsed>0) {
+					for(int k=1; k<=K; k++) {
+						if(map[nr][nc] - k < curLoc) {
+							map[nr][nc] -=k;
+							dfs(depth+1, nr, nc, kUsed-1);
+							map[nr][nc] +=k;
+						}
+					}
+				}
+					
+			}	
+		}
+		
+		if(depth>maxRoute) {
+			maxRoute = depth;
+			return;
+		}
 		
 	}
 	
-	public static void dfs(int y, int x, int count, int kAvl ) {
-		int curLoc = mountain[y][x];
-		
-		for(int i=0; i<4; i++) {
-			if(y + dy[i] < 0 || y+dy[i] >=N || x+dx[i] <0 || x+dx[i] >=N ) {
-				continue;
-			}
-			int nextLoc = mountain[y+dy[i]][x+dx[i]];
-			if(!visited[y+dy[i]][x+dx[i]] && nextLoc < curLoc) {
-				visited[y+dy[i]][x+dx[i]] = true;
-				dfs(y+dy[i], x+dx[i], count+1, kAvl);
-				visited[y+dy[i]][x+dx[i]] = false;
-			}
-			else if(kAvl>0) {
-				for(int j=1; j<=K; j++) {
-					if(!visited[y+dy[i]][x+dx[i]] && nextLoc-j < curLoc ) {
-						mountain[y+dy[i]][x+dx[i]] -=j;
-						dfs(y+dy[i], x+dx[i], count+1, kAvl-1);
-						mountain[y+dy[i]][x+dx[i]] +=j;
-					}
-				}
-			}
-			
-		}
-		if(count>maxRoute) {
-			maxRoute = count;
-		}
+	static boolean check(int r, int c) {
+		return (r>=0 && r<N && c>=0 && c<N && !visited[r][c]);
 	}
+
 }
