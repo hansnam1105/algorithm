e5ce99ff6b1222fd517cb242e0080a3d95f4a95e
HanSolo.CAU <34000255+hansnam1105@users.noreply.github.com>
2024-11-17 21:22:03 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1400 ms, Memory: 324740 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.IntStream;
 
 public class Main {
 
@@ -19,47 +18,56 @@ public class Main {
         }
     }
 
-    static int[] parent;
+    static int[] parent, rank;
     static ArrayList<Edge> edgeList;
 
-    public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-            edgeList = new ArrayList<>();
-            for (int i = 0; i < M; i++) {
-                st = new StringTokenizer(br.readLine());
-                int start = Integer.parseInt(st.nextToken());
-                int end = Integer.parseInt(st.nextToken());
-                int weight = Integer.parseInt(st.nextToken());
+        edgeList = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
 
-                edgeList.add(new Edge(start, end, weight));
-            }
+            edgeList.add(new Edge(start, end, weight));
+        }
 
-            parent = IntStream.rangeClosed(0, N).toArray();
+        parent = new int[N + 1];
+        rank = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
 
-            Collections.sort(edgeList);
+        Collections.sort(edgeList);
 
-            int totalWeight = 0;
-            int maxWeightInMST = 0;
-            for (Edge edge : edgeList) {
-                if (findRoot(edge.start) != findRoot(edge.end)) {
-                    totalWeight += edge.weight;
-                    unionSets(edge.start, edge.end);
-                    maxWeightInMST = edge.weight;
+        int totalWeight = 0;
+        int maxWeightInMST = 0;
+        int edgeCount = 0;
+        for (Edge edge : edgeList) {
+            if (findRoot(edge.start) != findRoot(edge.end)) {
+                totalWeight += edge.weight;
+                unionSets(edge.start, edge.end);
+                maxWeightInMST = edge.weight;
+                edgeCount++;
+
+                if (edgeCount == N - 1) {
+                    break;
                 }
             }
-
-            bw.write((totalWeight - maxWeightInMST) + "\n");
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
+        bw.write((totalWeight - maxWeightInMST) + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
     public static int findRoot(int x) {
@@ -72,8 +80,16 @@ public class Main {
     public static void unionSets(int x, int y) {
         x = findRoot(x);
         y = findRoot(y);
+
         if (x != y) {
-            parent[y] = x;
+            if (rank[x] > rank[y]) {
+                parent[y] = x;
+            } else if (rank[x] < rank[y]) {
+                parent[x] = y;
+            } else {
+                parent[y] = x;
+                rank[x]++;
+            }
         }
     }
 }
